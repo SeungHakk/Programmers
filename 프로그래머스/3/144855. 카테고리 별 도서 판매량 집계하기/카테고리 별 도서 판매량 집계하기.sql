6f323b5892dd04b06438d4a04e4eb39a47eb832f
SeungHakk <163830761+SeungHakk@users.noreply.github.com>
2024-04-08 17:22:31 +0900

    [level 3] Title: 카테고리 별 도서 판매량 집계하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/144855. 카테고리 별 도서 판매량 집계하기/카테고리 별 도서 판매량 집계하기.sql
+++ 프로그래머스/3/144855. 카테고리 별 도서 판매량 집계하기/카테고리 별 도서 판매량 집계하기.sql
@@ -1,20 +1,6 @@
-/*
-SELECT b.category, sum(bs.sales) TOTAL_SALES
-from book b, book_sales bs
-where b.book_id = bs.book_id
-    and bs.sales_date between to_char('2022-01-01','RRRR-MM-DD') and to_char('2022-01-31','RRRR-MM-DD')
-group by b.category
-order by 1; */
-
-
-
 SELECT b.category, sum(bs.sales) TOTAL_SALES
 from book b, book_sales bs
 where b.book_id = bs.book_id
     and bs.sales_date between to_date('2022-01-01','RRRR-MM-DD') and to_date('2022-01-31','RRRR-MM-DD')
 group by b.category
 order by 1;
-
-
-
-
